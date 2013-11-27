7bce49efa1173c281471e36c9c45d12a9c6b8b7f
Joe Madden <Joe@beardbro.com>
2013-11-27 17:38:37 -0500

    setting up initial testing page

---
+++ routes/testing.js
@@ -0,0 +1,6 @@
+function (req, res) {
+    var body = '<h1>This is a test of express app</h1><p>Seeing how it works.</p>';
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+};
