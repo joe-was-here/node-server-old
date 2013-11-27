4bc729f1f33a28014c84cf08f5403badd1d22d7d
Joe Madden <Joe@beardbro.com>
2013-11-27 17:47:32 -0500

    fixed export issue

--- routes/testing.js
+++ routes/testing.js
@@ -1,4 +1,4 @@
-function (req, res) {
+exports.testing = function (req, res) {
     var body = '<h1>This is a test of express app</h1><p>Seeing how it works.</p>';
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', body.length);
